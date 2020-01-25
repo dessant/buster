b04dc7e4d7a0d8e7db929f3360fd2959e3dc4de3
dessant <git@armin.dev>
2020-01-25 23:01:21 +0200

    chore: update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -79,7 +79,9 @@ module.exports = {
           {
             loader: 'sass-loader',
             options: {
-              includePaths: ['node_modules']
+              sassOptions: {
+                includePaths: ['node_modules']
+              }
             }
           }
         ]
