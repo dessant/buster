f3e09bfb82404bc90afeea4c79d09f976156da76
dessant <git@armin.dev>
2024-06-04 02:08:25 +0300

    feat: upgrade to Manifest V3 in Edge and Opera

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 const enableContributions =
   (process.env.ENABLE_CONTRIBUTIONS || 'true') === 'true';
 
-const mv3 = ['chrome'].includes(targetEnv);
+const mv3 = ['chrome', 'edge', 'opera'].includes(targetEnv);
 
 const distDir = path.join(__dirname, 'dist', targetEnv);
 
--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 const enableContributions =
   (process.env.ENABLE_CONTRIBUTIONS || 'true') === 'true';
 
-const mv3 = ['chrome'].includes(targetEnv);
+const mv3 = ['chrome', 'edge', 'opera'].includes(targetEnv);
 
 const provideExtApi = !['firefox', 'safari'].includes(targetEnv);
 
